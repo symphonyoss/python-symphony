9c1cfdce594019de2fce071865acb3e9abec8c37
Matt Joyce <matt.joyce@symphony.com>
2016-11-29 18:33:37 +0000

    version increment

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='python-symphony',
-    version='0.0.4',
+    version='0.0.5',
     description='python module for symphony chat',
     author='Matt Joyce',
     author_email='matt@nycresistor.com',
