4d8b91dc5b99307c36f525ea5dc36e20b8485498
Matt Joyce <matt.joyce@symphony.com>
2016-12-05 22:22:52 +0000

    base methods added to new agent class structure

---
+++ symphony/Agent/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    Purpose:
+        Agent API Methods
+'''
+
+__author__ = 'Matt Joyce'
+__email__ = 'matt@nycresistor.com'
+__copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
+
+import ast
+import json
+import requests
+import unicodedata
+
+
+class Agent():
+    from base import base 
+
+    def __init__(self, url, crt, key, session, keymngr):
+        self.__url__ = url
+        self.__crt__ = crt
+        self.__key__ = key
+        self.__session__ = session
+        self.__keymngr__ = keymngr
