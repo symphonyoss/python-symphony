f873608b14f2bd841a5fb8fcb14f9599689b7d4b
Matt Joyce <matt.joyce@symphony.com>
2016-12-05 22:25:58 +0000

    trying out new import style

--- symphony/__init__.py
+++ symphony/__init__.py
@@ -8,9 +8,10 @@ __author__ = 'Matt Joyce'
 __email__ = 'matt@nycresistor.com'
 __copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 
+import Agent
+import Pod
+
 from auth import Auth
-from agent import Agent
-from pod import Pod
 from mml import Mml
 from crypt import Crypt
 from config import Config
