bb788c126c06aa3756bb49dc08e7489c9fa9204a
Matt Joyce <matt.joyce@symphony.com>
2016-12-05 22:29:41 +0000

    root __init__.py cleanup

--- symphony/__init__.py
+++ symphony/__init__.py
@@ -9,10 +9,8 @@ __email__ = 'matt@nycresistor.com'
 __copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 
 import Agent
+import Auth
+import Config
+import Crypt
+import Mml
 import Pod
-
-from auth import Auth
-from mml import Mml
-from crypt import Crypt
-from config import Config
-from connections import Connections
