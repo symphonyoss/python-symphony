62192762468b6df3b8882b7bd18bc54a9ccaf7c6
Matt Joyce <matt.joyce@symphony.com>
2017-04-10 21:01:14 +0000

    full conversion of agent methods

--- symphony/Agent/base.py
+++ symphony/Agent/base.py
@@ -12,7 +12,6 @@ __copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 
 import ast
 import json
-import requests
 import symphony
 import unicodedata
 
@@ -23,9 +22,13 @@ def remove_control_characters(self, s):
 
 def test_echo(self, test_string):
     ''' echo test '''
+    # instantiate RESTful class()
     REST = symphony.RESTful()
+    # REST API method endpoint
     req_hook = 'agent/v1/util/echo'
+    # test message
     req_args = '{ "message": "'"%s"'" }' % test_string
+    # receive HTTP response code and response text
     status_code, response = REST.POST_query(req_hook, req_args)
     # return the token
     return status_code
@@ -33,65 +36,36 @@ def test_echo(self, test_string):
 
 def create_datafeed(self):
     ''' create datafeed '''
-    # https://your-pod.symphony.com/:8444/agent/v1/datafeed/create
-    headers = {'Content-Type': 'application/json',
-               'sessionToken': self.__session__,
-               'keyManagerToken': self.__keymngr__}
-
-    # HTTP POST query to keymanager authenticate API
-    try:
-        response = requests.post(self.__url__ + 'agent/v1/datafeed/create',
-                                 headers=headers,
-                                 cert=(self.__crt__, self.__key__),
-                                 verify=True)
-    except requests.exceptions.RequestException as e:
-        return e
+    # instantiate RESTful class()
+    REST = symphony.RESTful()
+    # REST API method endpoint
+    req_hook = 'agent/v1/datafeed/create'
+    req_args = None
+    status_code, response = REST.POST_query(req_hook, req_args)
     # load json response as list
-    datafeed = json.loads(response.text)
+    datafeed = json.loads(response)
     # return the token
     return datafeed['id']
 
 
 def read_datafeed(self, streamid):
     ''' get datafeed '''
-    headers = {'Content-Type': 'application/json',
-               'sessionToken': self.__session__,
-               'keyManagerToken': self.__keymngr__}
-
-    # HTTP POST query to keymanager authenticate API
-    try:
-        response = requests.get(self.__url__ + 'agent/v2/datafeed/' + str(streamid) + '/read',
-                                headers=headers,
-                                cert=(self.__crt__, self.__key__),
-                                verify=True)
-    except requests.exceptions.RequestException as e:
-        return e
-    # load json response as list
-    datafeed = response.text
-    datastat = response.status_code
-    if datastat == 200:
-        datafeed = ast.literal_eval(datafeed)
-    # return the token
-    return datafeed, datastat
+    # instantiate RESTful class()
+    REST = symphony.RESTful()
+    # REST API method endpoint
+    req_hook = 'agent/v1/datafeed/'
+    req_args = str(streamid) + '/read'
+    status_code, response = REST.GET_query(req_hook, req_args)
+    response = ast.literal_eval(response)
+    return response, status_code
 
 
 def send_message(self, threadid, msgFormat, message):
     ''' send message to threadid/stream '''
-    message = self.remove_control_characters(message)
-    headers = {'content-type': 'application/json',
-               'sessionToken': self.__session__,
-               'keyManagerToken': self.__keymngr__}
-    data = '{ "format": "%s", "message": "'"%s"'" }' % (msgFormat, message)
-    # HTTP POST query to keymanager authenticate API
-    try:
-        response = requests.post(self.__url__ + 'agent/v2/stream/' + threadid + '/message/create',
-                                 headers=headers,
-                                 data=data,
-                                 cert=(self.__crt__, self.__key__),
-                                 verify=True)
-    except requests.exceptions.RequestException as e:
-        return e
-    # load json response as list
-    string = response.text
-    # return the token
-    return string
+    # instantiate RESTful class()
+    REST = symphony.RESTful()
+    # REST API method endpoint
+    req_hook = 'agent/v2/stream/' + threadid + '/message/create'
+    req_args = '{ "format": "%s", "message": "'"%s"'" }' % (msgFormat, message)
+    status_code, response = REST.POST_query(req_hook, req_args)
+    return response
