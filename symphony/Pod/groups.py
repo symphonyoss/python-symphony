a647a56c4f3ff6c1c82a6b95b5529069896418b1
openfly <matt@nycresistor.com>
2017-04-14 15:15:25 -0400

    readd python stuff

--- symphony/Pod/groups.py
+++ symphony/Pod/groups.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
     Purpose:
         pod endpoint ib group methods
--- symphony/Pod/users.py
+++ symphony/Pod/users.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
     Purpose:
         pod endpoint user methods
