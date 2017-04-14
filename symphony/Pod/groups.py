8a27437601ed62eb159469f0163c6759a1497a49
openfly <matt@nycresistor.com>
2017-04-14 13:03:34 -0400

    add groups

--- symphony/Pod/groups.py
+++ symphony/Pod/groups.py
@@ -1,6 +1,6 @@
 '''
     Purpose:
-        pod endpoint group methods
+        pod endpoint ib group methods
 '''
 
 __author__ = 'Matt Joyce'
@@ -10,9 +10,24 @@ __copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 import json
 
 
-def group_list(self):
-    ''' group list '''
+def ib_group_list(self):
+    ''' ib group list '''
     req_hook = 'pod/v1/admin/group/list'
     req_args = None
     status_code, response = self.__rest__.GET_query(req_hook, req_args)
-    return json.loads(response)
+    return status_code, json.loads(response)
+
+
+def ib_group_member_list(self, group_id):
+    ''' ib group member list '''
+    req_hook = 'pod/v1/admin/group/' + group_id + '/membership/list'
+    req_args = None
+    status_code, response = self.__rest__.GET_query(req_hook, req_args)
+    return status_code, json.loads(response)
+
+
+def if_group_member_add(self, group_id, userids):
+    req_hook = 'pod/v1/admin/group/' + group_id + '/membership/add'
+    req_args = {'usersListId': userids}
+    status_code, response = self.__rest__.POST_query(req_hook, req_args)
+    return status_code, response
