373832a2f1a8b1efab2efa7f1c00c80e15ad07f1
openfly <matt@nycresistor.com>
2017-04-14 10:01:59 -0400

    more cleaning on Agent class

--- symphony/Agent/base.py
+++ symphony/Agent/base.py
@@ -21,22 +21,20 @@ def remove_control_characters(self, s):
 
 def test_echo(self, test_string):
     ''' echo test '''
-    # REST API method endpoint
     req_hook = 'agent/v1/util/echo'
     # test message
     req_args = '{ "message": "'"%s"'" }' % test_string
     # receive HTTP response code and response text
-    status_code, response = self.REST.POST_query(req_hook, req_args)
+    status_code, response = self.__rest__.POST_query(req_hook, req_args)
     # return the token
     return status_code
 
 
 def create_datafeed(self):
     ''' create datafeed '''
-    # REST API method endpoint
     req_hook = 'agent/v1/datafeed/create'
     req_args = None
-    status_code, response = self.REST.POST_query(req_hook, req_args)
+    status_code, response = self.__rest__.POST_query(req_hook, req_args)
     # load json response as list
     datafeed = json.loads(response)
     # return the token
@@ -45,18 +43,16 @@ def create_datafeed(self):
 
 def read_datafeed(self, streamid):
     ''' get datafeed '''
-    # REST API method endpoint
     req_hook = 'agent/v1/datafeed/' + str(streamid) + '/read'
     req_args = None
-    status_code, response = self.REST.GET_query(req_hook, req_args)
+    status_code, response = self.__rest__.GET_query(req_hook, req_args)
     response = ast.literal_eval(response)
     return response, status_code
 
 
 def send_message(self, threadid, msgFormat, message):
     ''' send message to threadid/stream '''
-    # REST API method endpoint
     req_hook = 'agent/v2/stream/' + threadid + '/message/create'
     req_args = '{ "format": "%s", "message": "'"%s"'" }' % (msgFormat, message)
-    status_code, response = self.REST.POST_query(req_hook, req_args)
+    status_code, response = self.__rest__.POST_query(req_hook, req_args)
     return response
