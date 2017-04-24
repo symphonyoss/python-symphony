4d2d3db3722b862bd82e08fed748f3c688b47bca
openfly <matt@nycresistor.com>
2017-04-24 11:55:41 -0400

    fix base / agent methods

--- symphony/Agent/base.py
+++ symphony/Agent/base.py
@@ -15,44 +15,45 @@ import json
 import unicodedata
 
 
-def remove_control_characters(self, s):
-    return "".join(ch for ch in s if unicodedata.category(ch)[0] != "C")
+class Base(object):
 
+    def __init__(self, *args, **kwargs):
+        super(Base, self).__init__(*args, **kwargs)
 
-def test_echo(self, test_string):
-    ''' echo test '''
-    req_hook = 'agent/v1/util/echo'
-    # test message
-    req_args = '{ "message": "'"%s"'" }' % test_string
-    # receive HTTP response code and response text
-    status_code, response = self.__rest__.POST_query(req_hook, req_args)
-    # return the token
-    return status_code, response
+    def remove_control_characters(self, s):
+        return "".join(ch for ch in s if unicodedata.category(ch)[0] != "C")
 
+    def test_echo(self, test_string):
+        ''' echo test '''
+        req_hook = 'agent/v1/util/echo'
+        # test message
+        req_args = '{ "message": "'"%s"'" }' % test_string
+        # receive HTTP response code and response text
+        status_code, response = self.__rest__.POST_query(req_hook, req_args)
+        # return the token
+        return status_code, response
 
-def create_datafeed(self):
-    ''' create datafeed '''
-    req_hook = 'agent/v1/datafeed/create'
-    req_args = None
-    status_code, response = self.__rest__.POST_query(req_hook, req_args)
-    # load json response as list
-    datafeed = json.loads(response)
-    # return the token
-    return status_code, datafeed['id']
+    def create_datafeed(self):
+        ''' create datafeed '''
+        req_hook = 'agent/v1/datafeed/create'
+        req_args = None
+        status_code, response = self.__rest__.POST_query(req_hook, req_args)
+        # load json response as list
+        datafeed = json.loads(response)
+        # return the token
+        return status_code, datafeed['id']
 
+    def read_datafeed(self, streamid):
+        ''' get datafeed '''
+        req_hook = 'agent/v1/datafeed/' + str(streamid) + '/read'
+        req_args = None
+        status_code, response = self.__rest__.GET_query(req_hook, req_args)
+        response = ast.literal_eval(response)
+        return status_code, response
 
-def read_datafeed(self, streamid):
-    ''' get datafeed '''
-    req_hook = 'agent/v1/datafeed/' + str(streamid) + '/read'
-    req_args = None
-    status_code, response = self.__rest__.GET_query(req_hook, req_args)
-    response = ast.literal_eval(response)
-    return status_code, response
-
-
-def send_message(self, threadid, msgFormat, message):
-    ''' send message to threadid/stream '''
-    req_hook = 'agent/v2/stream/' + threadid + '/message/create'
-    req_args = '{ "format": "%s", "message": "'"%s"'" }' % (msgFormat, message)
-    status_code, response = self.__rest__.POST_query(req_hook, req_args)
-    return status_code, response
+    def send_message(self, threadid, msgFormat, message):
+        ''' send message to threadid/stream '''
+        req_hook = 'agent/v2/stream/' + threadid + '/message/create'
+        req_args = '{ "format": "%s", "message": "'"%s"'" }' % (msgFormat, message)
+        status_code, response = self.__rest__.POST_query(req_hook, req_args)
+        return status_code, response
