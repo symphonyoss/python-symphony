412504e61232e2e2196c88b429b2490f50fa216a
openfly <matt@nycresistor.com>
2017-07-10 11:59:27 -0400

    rewrite __init__.py for Pod class to codegen stuff

--- symphony/Pod/__init__.py
+++ symphony/Pod/__init__.py
@@ -3,14 +3,15 @@
 
 '''
     Purpose:
-        pod methods
+        Pod API Methods
 '''
 
 __author__ = 'Matt Joyce'
 __email__ = 'matt@joyce.nyc'
 __copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 
-import symphony
+
+import symphonybinding
 
 from .users import Users
 from .streams import Streams
@@ -18,11 +19,14 @@ from .groups import Groups
 from .connections import Connections
 from .base import Base
 
-
 class Pod(Base, Users, Streams, Groups, Connections):
 
     def __init__(self, url, session, keymngr):
         self.__url__ = url
         self.__session__ = session
         self.__keymngr__ = keymngr
-        self.__rest__ = symphony.RESTful(self.__url__, self.__session__, self.__keymngr__)
+        try:
+            CG = symphonybinding.SymCodegen()
+            self.__pod__ = CG.pod_cg(self.__url__)
+        except Exception as err:
+            print err
